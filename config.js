f9adf418a0204378e30571668fa6968c311833f4
Loyrox <73528162+loyrox@users.noreply.github.com>
2022-10-06 20:08:38 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "ODAzMTk0OTkzODgzMjE3OTIw.G_Vx4L.TW8c_kMFFh-D2VaVJRiPiGwiPfK2EUK_5rxQ9k",
+    owner: "790251162774667265",
+    color: '#fff'
+}
---
+++ main.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js")
+const intents = new Discord.IntentsBitField(3276799)
+const bot = new Discord.Client({intents})
+const loadCommands = require("./loaders/loadCommands")
+const loadEvents = require("./loaders/loadEvents")
+const config = require("./config")
+
+bot.commands = new Discord.Collection();
+
+bot.color = "#ffff"
+
+bot.function = {
+    createId: require("./Fonctions/creatId"),
+    calculXp: require("./Fonctions/calculXp")
+}
+
+
+bot.on("ready", async () => {
+    
+    //console.log(`✅(${bot.user.username}) est désormait opérationel ✅`)
+    console.log(
+      `${"-".repeat(40)}\n` +
+      "|  Logs.  |\n" +
+      `${"-".repeat(40)}\n` +
+      "Bot Infos : \n" +
+      `Nom du bot    : ${bot.user.tag}!\n` +
+      `ID du bot     : ${bot.user.id}\n` +
+      `Invitation : https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958847 \n` +
+      `${"-".repeat(40)}\n`
+    );
+  
+  })
+
+bot.login(config.token)
+
+loadCommands(bot)
+loadEvents(bot)
+
+
+
+
